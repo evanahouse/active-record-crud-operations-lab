4474336e5cf1a436e9fd0872c92a6e09bbb2ac20
Evan <evanahouse@gmail.com>
2021-04-12 16:47:32 -0700

    completed assignment

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,10 @@
 require 'bundler/setup'
 Bundler.require
+ENV["SINATRA_ENV"] ||= "development"
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  database: "db/development.sqlite"
 )
 
 require_all 'app'
